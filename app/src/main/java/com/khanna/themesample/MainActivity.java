1b1bfa486f6d44efb5adfb1944276c7c62118978
gaurav.khanna <g24khanna@gmail.com>
2018-04-11 14:08:08 +0200

    Implemented dynamic theme example

--- app/src/main/java/com/khanna/themesample/MainActivity.java
+++ app/src/main/java/com/khanna/themesample/MainActivity.java
@@ -1,9 +1,8 @@
 package com.khanna.themesample;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,25 +11,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    public static final String INDIGO = "indigo";
+    public static final String PINK = "pink";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(getSavedTheme());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +35,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        TextView themeName = findViewById(R.id.theme_name);
+        themeName.setText(getSavedTheme() == R.style.AppTheme_NoActionBar_Indigo ? "Indigo" : "Pink");
     }
 
     @Override
@@ -79,23 +76,31 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.indigo) {
+            saveTheme(INDIGO);
+        } else if (id == R.id.pink) {
+            saveTheme(PINK);
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void saveTheme(String value) {
+        SharedPreferences.Editor editor = getPreferences(Activity.MODE_PRIVATE).edit();
+        editor.putString("theme", value);
+        editor.commit();
+        recreate();
+    }
+
+    private int getSavedTheme() {
+        String theme = getPreferences(Activity.MODE_PRIVATE).getString("theme", INDIGO);
+        switch (theme) {
+            case PINK:
+                return R.style.AppTheme_NoActionBar_Pink;
+            case INDIGO:
+            default:
+                return R.style.AppTheme_NoActionBar_Indigo;
+        }
     }
 }
